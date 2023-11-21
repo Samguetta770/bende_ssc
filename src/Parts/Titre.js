256c4e6660ff85f1650d1092a380a0f5bd4a1669
samos <samuelguetta@campus.technion.ac.il>
2023-11-21 16:17:17 +0200

    pop

--- src/Parts/Titre.js
+++ src/Parts/Titre.js
@@ -4,7 +4,7 @@ import './Titre.css'; // Importez le fichier CSS pour le style
 function Titre() {
   return (
     <div className="titre">
-        <h2>Installation de système solaire combiné</h2>
+        <h2>Installation de système solaire combiné TESTO</h2>
         <div className="ligne-sous-titre"></div>
         <p className="phrase-dessous">
             Effectuez votre devis en ligne gratuitement et sans engagement !
